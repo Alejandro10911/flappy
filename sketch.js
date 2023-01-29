bea2514ce3894375579bc89db63d0ce04054a71b
Alejandro <marcotenesaca80@gmail.com>
2023-01-29 02:53:16 -0500

    Adición de reset game, colisiones

--- sketch.js
+++ sketch.js
@@ -2,29 +2,23 @@ var bird;
 var pipes = [];
 var maxLines = 100;
 var wind = [];
-var increaseFR = 60;
 
 function setup() {
   createCanvas(1000, 600);
   bird = new Bird();
-  pipes.push(new Pipe());
+  //pipes.push(new Pipe());
   //wind.push(new Wind());
 }
 
 function draw() {
   background(0, 0, 220);
 
-  for (var i = pipes.length - 1; i >= 0; i--) {
+  for (var i = 0; i < pipes.length; i++) {
     pipes[i].show();
     pipes[i].update();
 
     if (pipes[i].hits(bird)) {
       console.log("HIT");
-    }
-
-
-    if (pipes[i].offscreen()) {
-      pipes.splice(i, 1);
     }
   }
 
@@ -44,30 +38,21 @@ function draw() {
   bird.show();
 
   // añadir nuevo pipe en base a al contador de frames 
-  if (frameCount % 40 == 0) {
+  if (frameCount % 50 == 0) {
     pipes.push(new Pipe());
-    //wind[i].increaseVelocity();
   }
-  /* if (frameCount % 500 == 0) {
-    wind[i].increaseVelocity();
-  } */
 
-  // tecla presionada para crear dash
-
-  /* while (num > 0) {
-    num = num - 1;
-    console.log(num);
-  } */
-
+  // tecla presionada para crear dash, incrementar velocidad del viento, 
+  // borrar elementos de array pipes[]
   if (keyIsPressed === true) {
     if (key == ' ') {
       bird.dashRigth();
       for (var i = 0; i < maxLines; i++) {
         wind[i].increaseVelocity();
       }
-      /* for (var i = pipes.length - 1; i >= 0; i--) {
-        pipes[i].increaseVelocity();
-      } */
+      for (var i = 0; i < pipes.lenght; i++) {
+        pipes.splice(i, 1);
+      }
     }
 
   }
@@ -77,7 +62,6 @@ function draw() {
     }
   }
 
-
 }
 function mouseClicked() {
   bird.up();
@@ -86,22 +70,23 @@ function mouseClicked() {
 // creación de los tubos
 function Pipe() {
 
-  var spacing = random(50, height / 2);
+  var spacing = 150;
   var centery = random(spacing, height - spacing);
 
   this.top = centery - spacing / 2;
   this.bottom = height - (centery + spacing / 2);
   this.x = width;
-  this.w = 50;
+  this.w = 80;
   this.speed = 6;
   this.velocityWhileDash = 12;
-
   this.highlight = false;
 
+
   this.hits = function (bird) {
-    if (bird.y < this.top || bird.y > height - this.bottom) {
-      if (bird.x > this.x && bird.x < this.x + this.w) {
+    if (bird.y <= this.top + 40 || bird.y >= height - this.bottom - 40) {
+      if (bird.x >= this.x - 10 && bird.x <= this.x + this.w + 40) {
         this.highlight = true;
+        bird.resetGame();
         return true;
       }
     }
@@ -113,7 +98,7 @@ function Pipe() {
     noStroke();
     fill(180, 0, 255);
     if (this.highlight) {
-      fill(255, 0, 0);
+      fill(255);
     }
     rect(this.x, 0, this.w, this.top);
     rect(this.x, height - this.bottom, this.w, this.bottom);
@@ -123,23 +108,11 @@ function Pipe() {
     this.x -= this.speed;
   }
 
-  this.offscreen = function () {
-    if (this.x < -this.w) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  // incrementar velocidad de los tubos mientras sucede el dash
-  /* this.increaseVelocity = function () {
-    this.x -= this.velocityWhileDash;
-  } */
 }
 
 //creación de bird
 function Bird() {
-  this.y = height / 2;
+  this.y = height / 4;
   this.x = width / 2 + width / 4;
 
   this.gravity = 0.6;
@@ -151,10 +124,16 @@ function Bird() {
   this.green = 255;
   this.blue = 255;
 
+  this.resetGame = function () {
+    this.y = -40;
+    this.x = width / 2 + width / 4;
+    pipes = [];
+  }
+
   this.show = function () {
     fill(this.red, this.green, this.blue);
     noStroke();
-    ellipse(this.x, this.y, 50, 50);
+    circle(this.x, this.y, 60);
   }
 
   this.up = function () {
@@ -167,15 +146,10 @@ function Bird() {
     this.y += this.velocity;
     this.x -= this.force / 30;
 
-    if (this.y > height) {
-      this.y = height;
-      this.velocity = 0;
+    if (this.y > height || this.x < 0) {
+      this.resetGame();
     }
 
-    if (this.y < 0) {
-      this.y = 0;
-      this.velocity = 0;
-    }
   }
 
   // mecánica dash
@@ -197,6 +171,11 @@ function Wind() {
   this.lenght = 50;
   this.velocity = 15;
   this.velocityWhileDash = 40;
+
+  this.resetGame = function () {
+    this.x = random(20, width);
+    this.y = random(height);
+  }
 
   this.update = function () {
     this.x -= this.velocity;
