fe141d68f288ce46bfbd993cc24819ce745edbdd
Alejandro <marcotenesaca80@gmail.com>
2023-01-29 19:31:37 -0500

    Nuevos cambios y adición de sonido de fondo

--- sketch.js
+++ sketch.js
@@ -1,11 +1,17 @@
 var bird;
 var pipes = [];
-var maxLines = 100;
 var wind = [];
+var soundAmbience;
+
+function preload() {
+  soundAmbience = loadSound('sounds/ambience.mp3');
+  pipes.push(new Pipe());
+}
 
 function setup() {
   createCanvas(1000, 600);
   bird = new Bird();
+  soundAmbience.loop();
   //pipes.push(new Pipe());
   //wind.push(new Wind());
 }
@@ -13,33 +19,43 @@ function setup() {
 function draw() {
   background(0, 0, 220);
 
-  for (var i = 0; i < pipes.length; i++) {
+  for (var i = pipes.length - 1; i >= 0; i--) {
     pipes[i].show();
     pipes[i].update();
 
     if (pipes[i].hits(bird)) {
       console.log("HIT");
     }
-  }
 
-  // loop del viento
-  for (var i = 0; i < maxLines; i++) {
-    wind.push(new Wind());
-  }
+    if (pipes[i].offscreen()) {
+      pipes.splice(i, 1);
+    }
 
-  //loop de los tubos
-  for (var i = 0; i < maxLines; i++) {
+  }
+  console.log(wind.length);
+  console.log(pipes.length);
+
+  for (var i = wind.length - 1; i >= 0; i--) {
     wind[i].update();
     wind[i].display();
+
+    if (wind[i].offscreen()) {
+      wind.splice(i, 1);
+    }
 
   }
 
   bird.update();
   bird.show();
 
-  // añadir nuevo pipe en base a al contador de frames 
+  // añadir nuevo pipe en base al contador de frames 
   if (frameCount % 50 == 0) {
     pipes.push(new Pipe());
+  }
+
+  // añadir nuevo wind en base al contador de frames
+  if (frameCount % 1 == 0) {
+    wind.push(new Wind());
   }
 
   // tecla presionada para crear dash, incrementar velocidad del viento, 
@@ -47,11 +63,8 @@ function draw() {
   if (keyIsPressed === true) {
     if (key == ' ') {
       bird.dashRigth();
-      for (var i = 0; i < maxLines; i++) {
+      for (var i = wind.length - 1; i >= 0; i--) {
         wind[i].increaseVelocity();
-      }
-      for (var i = 0; i < pipes.lenght; i++) {
-        pipes.splice(i, 1);
       }
     }
 
@@ -79,33 +92,34 @@ function Pipe() {
   this.w = 80;
   this.speed = 6;
   this.velocityWhileDash = 12;
-  this.highlight = false;
+  this.valueOutScreen = 0;
 
 
   this.hits = function (bird) {
     if (bird.y <= this.top + 40 || bird.y >= height - this.bottom - 40) {
       if (bird.x >= this.x - 10 && bird.x <= this.x + this.w + 40) {
-        this.highlight = true;
         bird.resetGame();
-        return true;
       }
     }
-    this.highlight = false;
-    return false;
   }
 
   this.show = function () {
     noStroke();
     fill(180, 0, 255);
-    if (this.highlight) {
-      fill(255);
-    }
     rect(this.x, 0, this.w, this.top);
     rect(this.x, height - this.bottom, this.w, this.bottom);
   }
 
   this.update = function () {
     this.x -= this.speed;
+  }
+
+  this.offscreen = function () {
+    if (this.x < - this.w) {
+      return true
+    } else {
+      return false
+    }
   }
 
 }
@@ -127,7 +141,8 @@ function Bird() {
   this.resetGame = function () {
     this.y = -40;
     this.x = width / 2 + width / 4;
-    pipes = [];
+    //pipes = [];
+    //wind = [];
   }
 
   this.show = function () {
@@ -166,23 +181,15 @@ function Bird() {
 //creación de el viento
 function Wind() {
 
-  this.x = random(20, width);
+  this.x = random(width + 20, width + 50);
   this.y = random(height);
-  this.lenght = 50;
+  this.lenght = 80;
   this.velocity = 15;
   this.velocityWhileDash = 40;
-
-  this.resetGame = function () {
-    this.x = random(20, width);
-    this.y = random(height);
-  }
+  this.w = 0;
 
   this.update = function () {
     this.x -= this.velocity;
-
-    if (this.x < 0) {
-      this.x = random(width + 20, width * 2);
-    }
   }
 
   this.display = function () {
@@ -194,6 +201,14 @@ function Wind() {
   // incrementar velocidad del viento mientras sucede el dash
   this.increaseVelocity = function () {
     this.x -= this.velocityWhileDash;
+  }
+
+  this.offscreen = function () {
+    if (this.x < - this.w) {
+      return true
+    } else {
+      return false
+    }
   }
 }
 
